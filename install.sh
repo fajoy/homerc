0b514b632cf7706162444aa27c066207b8b5d68c
fajoy <wuminfajoy@gmail.com>
2013-03-28 15:07:05 +0800

    add install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+cd `dirname $0`
+now=`date "+%Y%m%d%H%M%S"`
+pwd=`pwd`
+change_file(){
+    hrc=${HOME}/${rc}
+    if [ -e ${hrc} ] ;then
+        if [ -z "`diff -q ${rc} $hrc`"  ] ;then
+            echo "[Keep]    ${hrc}"
+            return
+        fi
+        echo "[Update]  ${hrc}"
+        mv ${hrc} ${hrc}.${now}.bak
+    else
+        echo "[Create]  ${hrc}"
+    fi
+    cp ${pwd}/${rc} ${hrc}
+}
+for rc in .*rc ;
+do
+change_file ${rc}
+done
